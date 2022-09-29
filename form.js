4c54784f0df8c2edad53bae9b4b4c8f446cb6afe
tonight11 <godofspeedman@gmail.com>
2022-09-29 11:56:29 +0300

    added thank html

--- form.js
+++ form.js
@@ -1,13 +1,19 @@
-const pass_field = document.querySelector(".pass");
-const showBtn = document.querySelector(".show");
-showBtn.addEventListener("click", function () {
-  if (pass_field.type === "password") {
-    pass_field.type = "text";
-    showBtn.textContent = "HIDE";
-    showBtn.style.color = "#D052F6";
-  } else {
-    pass_field.type = "password";
-    showBtn.textContent = "SHOW";
-    showBtn.style.color = "#222";
-  }
+const pass_field = document.querySelector('.pass');
+const showBtn = document.querySelector('.show');
+const form = document.querySelector('.form-submit');
+
+showBtn.addEventListener('click', function () {
+	if (pass_field.type === 'password') {
+		pass_field.type = 'text';
+		showBtn.textContent = 'HIDE';
+		showBtn.style.color = '#D052F6';
+	} else {
+		pass_field.type = 'password';
+		showBtn.textContent = 'SHOW';
+		showBtn.style.color = '#222';
+	}
+});
+
+form.addEventListener('click', e => {
+	console.log(e);
 });
